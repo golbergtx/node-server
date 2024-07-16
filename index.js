9fbcecab126fcc60162295c866474f3893b939a1
Danil <golbergdon@gmail.com>
2024-07-16 16:24:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const server = http2.createSecureServer({
     key: fs.readFileSync('localhost-private.pem'),  // Path to your SSL key file
     cert: fs.readFileSync('localhost-cert.pem'), // Path to your SSL certificate file
 }, (req) => {
-    console.log('zalupa');
     const ip = req.socket.remoteAddress;
     const port = req.socket.remotePort;
-    console.log(ip);
-    console.log(port);
-    // console.log(req.socket?.remoteAddress);
-    // console.log(req.connection.socket.remoteAddress);
-    console.log(req);
+    console.log(`ip: ${ip}`);
+    console.log(`port: ${port}`);
 });
 
 server.on('error', (err) => {
@@ -24,16 +20,60 @@ server.on('settings', (settings) => {
 });
 
 server.on('stream', (stream, headers) => {
-    // console.log('headers');
-    // console.log(headers);
+    console.log();
+
+    const htmlTable = createTableFromJSON(JSON.parse(JSON.stringify(headers)));
 
     stream.respond({
         'content-type': 'text/html',
         ':status': 200,
     });
-    stream.end('<html><head><title>HTTP/2 Server</title></head><body><h1>Hello, HTTP/2!</h1></body></html>');
+
+    stream.end(
+        `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>JSON to HTML Table</title>
+                <style>
+                    table {
+                        width: 100%;
+                        border-collapse: collapse;
+                    }
+                    th, td {
+                        border: 1px solid black;
+                        padding: 8px;
+                        text-align: left;
+                    }
+                    th {
+                        background-color: #f2f2f2;
+                    }
+                </style>
+            </head>
+            <body>
+                <div id="table-container">${htmlTable}</div>
+            </body>
+            </html>
+        `
+    );
 });
 
 server.listen(8443, () => {
     console.log('Server is running on https://localhost:8443');
 });
+
+function createTableFromJSON(jsonData) {
+    let table = '<table border="1">';
+    table += '<tr><th>Key</th><th>Value</th></tr>';
+
+    for (let key in jsonData) {
+        if (jsonData.hasOwnProperty(key)) {
+            table += `<tr><td>${key}</td><td>${jsonData[key]}</td></tr>`;
+        }
+    }
+
+    table += '</table>';
+    return table;
+}
